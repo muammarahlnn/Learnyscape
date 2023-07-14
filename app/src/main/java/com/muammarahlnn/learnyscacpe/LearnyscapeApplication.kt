7ccef5610721255a9e87162ee46c165cd77c8a64
muammarahlnn <21muammarahlan@gmail.com>
2023-07-14 14:51:43 +0800

    feat: add LearnyscapeApplication

---
+++ app/src/main/java/com/muammarahlnn/learnyscacpe/LearnyscapeApplication.kt
@@ -0,0 +1,12 @@
+package com.muammarahlnn.learnyscacpe
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+
+/**
+ * @author Muammar Ahlan Abimanyu (muammarahlnn)
+ * @file LearnyscapeApplication, 14/07/2023 14.49 by Muammar Ahlan Abimanyu
+ */
+@HiltAndroidApp
+class LearnyscapeApplication : Application()
