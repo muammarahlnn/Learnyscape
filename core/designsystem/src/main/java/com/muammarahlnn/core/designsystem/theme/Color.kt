3a3a51fe6d943014037192ba27a9b8e0b6146607
muammarahlnn <21muammarahlan@gmail.com>
2023-07-14 21:21:54 +0800

    feat: add Learnyscape base colors

---
+++ core/designsystem/src/main/java/com/muammarahlnn/core/designsystem/theme/Color.kt
@@ -0,0 +1,17 @@
+package com.muammarahlnn.core.designsystem.theme
+
+import androidx.compose.ui.graphics.Color
+
+
+/**
+ * @author Muammar Ahlan Abimanyu (muammarahlnn)
+ * @file Color, 14/07/2023 21.13 by Muammar Ahlan Abimanyu
+ */
+
+
+internal val White = Color(0xFFFFFFFF)
+internal val WhiteSmoke = Color(0xFFF5F5F5)
+internal val Red = Color(0xFFDF2E38)
+internal val Blue = Color(0xFF0079FF)
+internal val Black = Color(0xFF2D2727)
+internal val Gray = Color(0xFF6A6A6A)
