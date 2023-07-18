f7956d56fca71bb45555017c55cee67ee2ed000a
muammarahlnn <21muammarahlan@gmail.com>
2023-07-18 16:38:18 +0800

    feat: add LearnyscapeIcons

---
+++ core/designsystem/src/main/java/com/muammarahlnn/core/designsystem/icon/LearnyscapeIcons.kt
@@ -0,0 +1,19 @@
+package com.muammarahlnn.core.designsystem.icon
+
+import com.muammarahlnn.core.designsystem.R
+
+
+/**
+ * @author Muammar Ahlan Abimanyu (muammarahlnn)
+ * @file LearnyscapeIcons, 18/07/2023 16.34 by Muammar Ahlan Abimanyu
+ */
+object LearnyscapeIcons {
+    val Home = R.drawable.ic_home
+    val HomeBorder = R.drawable.ic_home_border
+    val Search = R.drawable.ic_search
+    val SearchBorder = R.drawable.ic_search_border
+    val CalendarToday = R.drawable.ic_calendar_today
+    val CalendarTodayBorder = R.drawable.ic_calendary_today_border
+    val Person = R.drawable.ic_person
+    val PersonBorder = R.drawable.ic_person_border
+}
