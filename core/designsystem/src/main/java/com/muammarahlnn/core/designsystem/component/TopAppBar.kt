68a37f97685d2abe98a245bc37eee54f48cb27b0
muammarahlnn <21muammarahlan@gmail.com>
2023-07-25 14:30:20 +0800

    feat(app): add Learnyscape base top app bar

--- core/designsystem/src/main/java/com/muammarahlnn/core/designsystem/component/TopAppBar.kt
+++ core/designsystem/src/main/java/com/muammarahlnn/core/designsystem/component/TopAppBar.kt
@@ -1,15 +1,82 @@
 package com.muammarahlnn.core.designsystem.component
 
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarColors
 import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 
 
 /**
  * @author Muammar Ahlan Abimanyu (muammarahlnn)
  * @file TopAppBar, 25/07/2023 13.08 by Muammar Ahlan Abimanyu
  */
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun LearnyscapeTopAppBar(
+    title: @Composable () -> Unit,
+    @DrawableRes actionIconRes: Int,
+    actionIconContentDescription: String?,
+    modifier: Modifier = Modifier,
+    colors: TopAppBarColors = TopAppBarDefaults.topAppBarColors(),
+    scrollBehavior: TopAppBarScrollBehavior? = null,
+    onActionClick: () -> Unit = {},
+) {
+    TopAppBar(
+        title = title,
+        actions = {
+            IconButton(onClick = onActionClick) {
+                Icon(
+                    painterResource(id = actionIconRes),
+                    contentDescription = actionIconContentDescription,
+                )
+            }
+        },
+        colors = colors,
+        scrollBehavior = scrollBehavior,
+        modifier = modifier,
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun LearnyscapeTopAppBar(
+    @StringRes titleRes: Int,
+    @DrawableRes navigationIconRes: Int,
+    navigationIconContentDescription: String?,
+    modifier: Modifier = Modifier,
+    colors: TopAppBarColors = TopAppBarDefaults.topAppBarColors(),
+    scrollBehavior: TopAppBarScrollBehavior? = null,
+    onNavigationClick: () -> Unit = {},
+) {
+    TopAppBar(
+        title = {
+            Text(text = stringResource(id = titleRes))
+        },
+        navigationIcon = {
+            IconButton(onClick = onNavigationClick) {
+                Icon(
+                    painterResource(id = navigationIconRes),
+                    contentDescription = navigationIconContentDescription,
+                )
+            }
+        },
+        colors = colors,
+        scrollBehavior = scrollBehavior,
+        modifier = modifier,
+    )
+}
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
