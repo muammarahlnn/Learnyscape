2ea2d7ea8e96d51569e4399a84eb5419679684e2
muammarahlnn <21muammarahlan@gmail.com>
2023-07-25 15:46:22 +0800

    feat(search): add SearchTopAppBar

--- feature/search/src/main/java/com/muammarahlnn/learnyscape/feature/search/SearchScreen.kt
+++ feature/search/src/main/java/com/muammarahlnn/learnyscape/feature/search/SearchScreen.kt
@@ -1,13 +1,18 @@
 package com.muammarahlnn.learnyscape.feature.search
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import com.muammarahlnn.core.designsystem.component.LearnyscapeTopAppBar
+import com.muammarahlnn.core.designsystem.component.defaultTopAppBarColors
 
 
 /**
@@ -15,16 +20,33 @@ import androidx.compose.ui.res.stringResource
  * @file SearchScreen, 20/07/2023 22.06 by Muammar Ahlan Abimanyu
  */
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 internal fun SearchScreen(modifier: Modifier = Modifier) {
-    Box(
-        contentAlignment = Alignment.Center,
-        modifier = modifier
-            .fillMaxSize()
-    ) {
-        Text(
-            text = stringResource(id = R.string.search),
-            color = MaterialTheme.colorScheme.onBackground
-        )
+    Column(modifier = modifier) {
+        SearchTopAppBar()
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = Modifier.fillMaxSize()
+        ) {
+            Text(
+                text = stringResource(id = R.string.search),
+                color = MaterialTheme.colorScheme.onBackground
+            )
+        }
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun SearchTopAppBar(
+    modifier: Modifier = Modifier,
+    scrollBehavior: TopAppBarScrollBehavior? = null,
+) {
+    LearnyscapeTopAppBar(
+        title = R.string.search,
+        scrollBehavior = scrollBehavior,
+        colors = defaultTopAppBarColors(),
+        modifier = modifier
+    )
 }
