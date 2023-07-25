ebe87216b6572815fee6792e28e7aba3c595a6ec
muammarahlnn <21muammarahlan@gmail.com>
2023-07-25 16:02:10 +0800

    feat(profile): add ProfileTopAppBar

--- feature/profile/src/main/java/com/muammarahlnn/learnyscape/feature/profile/ProfileScreen.kt
+++ feature/profile/src/main/java/com/muammarahlnn/learnyscape/feature/profile/ProfileScreen.kt
@@ -1,13 +1,18 @@
 package com.muammarahlnn.learnyscape.feature.profile
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import com.muammarahlnn.core.designsystem.component.LearnyscapeTopAppBar
+import com.muammarahlnn.core.designsystem.component.defaultTopAppBarColors
 
 
 /**
@@ -15,16 +20,34 @@ import androidx.compose.ui.res.stringResource
  * @file ProfileScreen, 20/07/2023 21.48 by Muammar Ahlan Abimanyu
  */
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 internal fun ProfileScreen(modifier: Modifier = Modifier) {
-    Box(
-        contentAlignment = Alignment.Center,
-        modifier = modifier
-            .fillMaxSize()
-    ) {
-        Text(
-            text = stringResource(id = R.string.profile),
-            color = MaterialTheme.colorScheme.onBackground
-        )
+    Column(modifier = modifier) {
+        ProfileTopAppBar()
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = modifier
+                .fillMaxSize()
+        ) {
+            Text(
+                text = stringResource(id = R.string.profile),
+                color = MaterialTheme.colorScheme.onBackground
+            )
+        }
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun ProfileTopAppBar(
+    modifier: Modifier = Modifier,
+    scrollBehavior: TopAppBarScrollBehavior? = null,
+) {
+    LearnyscapeTopAppBar(
+        title = R.string.profile,
+        scrollBehavior = scrollBehavior,
+        colors = defaultTopAppBarColors(),
+        modifier = modifier,
+    )
 }
