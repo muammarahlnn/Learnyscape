96f847aef2c88ab8297b0b5a7614ed294b9b871f
muammarahlnn <21muammarahlan@gmail.com>
2023-07-28 17:11:12 +0800

    feat(search): add SearchTextField

--- feature/search/src/main/java/com/muammarahlnn/learnyscape/feature/search/SearchScreen.kt
+++ feature/search/src/main/java/com/muammarahlnn/learnyscape/feature/search/SearchScreen.kt
@@ -1,18 +1,42 @@
 package com.muammarahlnn.learnyscape.feature.search
 
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.input.key.Key
+import androidx.compose.ui.input.key.key
+import androidx.compose.ui.input.key.onKeyEvent
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.unit.dp
 import com.muammarahlnn.core.designsystem.component.LearnyscapeTopAppBar
 import com.muammarahlnn.core.designsystem.component.defaultTopAppBarColors
+import com.muammarahlnn.core.designsystem.R as designSystemR
 
 
 /**
@@ -23,17 +47,18 @@ import com.muammarahlnn.core.designsystem.component.defaultTopAppBarColors
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 internal fun SearchScreen(modifier: Modifier = Modifier) {
-    Column(modifier = modifier) {
+    Column(modifier = modifier.fillMaxSize()) {
         SearchTopAppBar()
-        Box(
-            contentAlignment = Alignment.Center,
-            modifier = Modifier.fillMaxSize()
-        ) {
-            Text(
-                text = stringResource(id = R.string.search),
-                color = MaterialTheme.colorScheme.onBackground
-            )
-        }
+        SearchTextField()
+//        Box(
+//            contentAlignment = Alignment.Center,
+//            modifier = Modifier.fillMaxSize()
+//        ) {
+//            Text(
+//                text = stringResource(id = R.string.search),
+//                color = MaterialTheme.colorScheme.onBackground
+//            )
+//        }
     }
 }
 
@@ -49,4 +74,87 @@ private fun SearchTopAppBar(
         colors = defaultTopAppBarColors(),
         modifier = modifier
     )
+}
+
+@OptIn(ExperimentalComposeUiApi::class)
+@Composable
+private fun SearchTextField() {
+    val focusRequester = remember { FocusRequester() }
+    val keyboardController = LocalSoftwareKeyboardController.current
+
+    val onSearchExplicitlyTriggered = {
+        keyboardController?.hide()
+    }
+
+    var searchQuery by rememberSaveable { mutableStateOf("") }
+    OutlinedTextField(
+        value = searchQuery,
+        onValueChange = {
+            searchQuery = it
+        },
+        placeholder = {
+            Text(text = stringResource(id = R.string.search_text_field_placeholder))
+        },
+        leadingIcon = {
+            Icon(
+                painter = painterResource(id = R.drawable.ic_search_border),
+                contentDescription = stringResource(id = R.string.search),
+            )
+        },
+        trailingIcon = {
+            if (searchQuery.isNotEmpty()) {
+                IconButton(
+                    onClick = {
+                        searchQuery = ""
+                    }
+                ) {
+                    Icon(
+                        painter = painterResource(id = designSystemR.drawable.ic_close),
+                        contentDescription = stringResource(
+                            id = R.string.search_text_field_clear_icon_description
+                        ),
+                    )
+                }
+            }
+        },
+        singleLine = true,
+        shape = RoundedCornerShape(10.dp),
+        colors = OutlinedTextFieldDefaults.colors(
+            cursorColor = MaterialTheme.colorScheme.onSurfaceVariant,
+            unfocusedBorderColor = MaterialTheme.colorScheme.onSurfaceVariant,
+            unfocusedLeadingIconColor = MaterialTheme.colorScheme.onSurfaceVariant,
+            unfocusedTrailingIconColor = MaterialTheme.colorScheme.onSurfaceVariant ,
+            unfocusedPlaceholderColor = MaterialTheme.colorScheme.onSurfaceVariant,
+            unfocusedTextColor = MaterialTheme.colorScheme.onSurfaceVariant,
+            focusedBorderColor = MaterialTheme.colorScheme.onSurface,
+            focusedLeadingIconColor = MaterialTheme.colorScheme.onSurface,
+            focusedTrailingIconColor = MaterialTheme.colorScheme.onSurface,
+            focusedPlaceholderColor = MaterialTheme.colorScheme.onSurface,
+            focusedTextColor = MaterialTheme.colorScheme.onSurface,
+        ),
+        keyboardOptions = KeyboardOptions(
+            imeAction = ImeAction.Search,
+        ),
+        keyboardActions = KeyboardActions(
+            onSearch = {
+                onSearchExplicitlyTriggered()
+            }
+        ),
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp)
+            .focusRequester(focusRequester)
+            .onKeyEvent {
+                if (it.key == Key.Enter) {
+                    onSearchExplicitlyTriggered()
+                    true
+                } else {
+                    false
+                }
+            },
+    )
+
+    LaunchedEffect(Unit) {
+        focusRequester.requestFocus()
+    }
 }
