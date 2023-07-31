adc828d5a7b0e617af73fd61c1407915d6399d6d
muammarahlnn <21muammarahlan@gmail.com>
2023-07-31 19:36:32 +0800

    feat(profile): add profile content

--- feature/profile/src/main/java/com/muammarahlnn/learnyscape/feature/profile/ProfileScreen.kt
+++ feature/profile/src/main/java/com/muammarahlnn/learnyscape/feature/profile/ProfileScreen.kt
@@ -1,16 +1,32 @@
 package com.muammarahlnn.learnyscape.feature.profile
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FilledIconButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import com.muammarahlnn.core.designsystem.component.LearnyscapeTopAppBar
 import com.muammarahlnn.core.designsystem.component.defaultTopAppBarColors
 
@@ -20,21 +36,19 @@ import com.muammarahlnn.core.designsystem.component.defaultTopAppBarColors
  * @file ProfileScreen, 20/07/2023 21.48 by Muammar Ahlan Abimanyu
  */
 
+@Composable
+internal fun ProfileRoute(
+    modifier: Modifier = Modifier,
+) {
+    ProfileScreen(modifier = modifier)
+}
+
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-internal fun ProfileScreen(modifier: Modifier = Modifier) {
-    Column(modifier = modifier) {
+private fun ProfileScreen(modifier: Modifier = Modifier) {
+    Column(modifier = modifier.fillMaxSize()) {
         ProfileTopAppBar()
-        Box(
-            contentAlignment = Alignment.Center,
-            modifier = modifier
-                .fillMaxSize()
-        ) {
-            Text(
-                text = stringResource(id = R.string.profile),
-                color = MaterialTheme.colorScheme.onBackground
-            )
-        }
+        ProfileContent()
     }
 }
 
@@ -50,4 +64,77 @@ private fun ProfileTopAppBar(
         colors = defaultTopAppBarColors(),
         modifier = modifier,
     )
+}
+
+@Composable
+private fun ProfileContent(
+    modifier: Modifier = Modifier,
+) {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = modifier
+            .fillMaxSize()
+            .padding(32.dp)
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Box {
+                Image(
+                    painter = painterResource(id = R.drawable.ava_luffy),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(160.dp)
+                        .clip(CircleShape)
+                )
+                FilledIconButton(
+                    colors = IconButtonDefaults.filledIconButtonColors(
+                        containerColor = MaterialTheme.colorScheme.primary,
+                        contentColor = MaterialTheme.colorScheme.onPrimary,
+                    ),
+                    onClick = {
+                        // will implement later
+                    },
+                    modifier = Modifier
+                        .align(Alignment.BottomEnd)
+                        .size(48.dp)
+                ) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_photo_camera),
+                        contentDescription = stringResource(id = R.string.camera_icon_description),
+                    )
+                }
+            }
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            // hardcoded text just for dummy purpose
+            Text(
+                text = "Muammar Ahlan Abimanyu",
+                style = MaterialTheme.typography.titleMedium
+            )
+            Text(
+                text = "H071191032",
+                style = MaterialTheme.typography.bodyLarge
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Button(
+                onClick = {
+                    // will implement later
+                },
+                shape = RoundedCornerShape(10.dp),
+                modifier = Modifier
+                    .fillMaxWidth()
+            ) {
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_logout),
+                    contentDescription = stringResource(id = R.string.logout_icon_description)
+                )
+                Spacer(modifier = Modifier.size(ButtonDefaults.IconSpacing))
+                Text(text = stringResource(id = R.string.logout))
+            }
+        }
+    }
 }
