70504fa05254dc9773eafc9ecd8fb64c866bc067
muammarahlnn <21muammarahlan@gmail.com>
2023-08-01 23:13:23 +0800

    feat(notifications): add NotificationContent

--- feature/notifications/src/main/java/com/muammarahlnn/learnyscape/feature/notifications/NotificationsScreen.kt
+++ feature/notifications/src/main/java/com/muammarahlnn/learnyscape/feature/notifications/NotificationsScreen.kt
@@ -1,8 +1,23 @@
 package com.muammarahlnn.learnyscape.feature.notifications
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -10,7 +25,11 @@ import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
 import com.muammarahlnn.learnyscape.core.designsystem.component.LearnyscapeTopAppBar
 import com.muammarahlnn.learnyscape.core.designsystem.component.defaultTopAppBarColors
 import com.muammarahlnn.learnyscape.core.designsystem.R as designSystemR
@@ -33,16 +52,29 @@ internal fun NotificationsRoute(
 private fun NotificationsScreen(
     modifier: Modifier = Modifier,
 ) {
-    Column(modifier = modifier) {
+    Column(modifier = modifier.fillMaxSize()) {
         NotificationsTopAppBar()
-        Box(
-            contentAlignment = Alignment.Center,
-            modifier = Modifier.fillMaxSize()
-        ) {
-            Text(
-                text = stringResource(id = R.string.notifications),
-                color = MaterialTheme.colorScheme.onBackground
-            )
+        NotificationContent()
+    }
+}
+
+@Composable
+private fun NotificationContent(
+    modifier: Modifier = Modifier,
+) {
+    LazyColumn(
+        contentPadding = PaddingValues(16.dp),
+        verticalArrangement = Arrangement.spacedBy(6.dp),
+        modifier = modifier,
+    ) {
+        item {
+            NotificationItem(type = NotificationType.MODULE)
+        }
+        item {
+            NotificationItem(type = NotificationType.ASSIGNMENT)
+        }
+        item {
+            NotificationItem(type = NotificationType.QUIZ)
         }
     }
 }
@@ -63,4 +95,82 @@ private fun NotificationsTopAppBar(
         scrollBehavior = scrollBehavior,
         modifier = modifier,
     )
+}
+
+@Composable
+private fun NotificationItem(
+    type: NotificationType,
+    modifier: Modifier = Modifier,
+) {
+    Card(
+        shape = RoundedCornerShape(10.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.onPrimary,
+        ),
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 2.dp,
+        ),
+        modifier = modifier
+            .fillMaxWidth()
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.padding(10.dp)
+        ) {
+            Box(
+                contentAlignment = Alignment.Center,
+                modifier = Modifier
+                    .clip(CircleShape)
+                    .background(MaterialTheme.colorScheme.primary)
+                    .padding(10.dp)
+            ) {
+                Image(
+                    painter = getNotificationIcon(type = type),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(20.dp)
+                )
+            }
+
+            Spacer(modifier = Modifier.width(8.dp))
+
+            Column(
+                modifier = Modifier.weight(1f)
+            ) {
+                Text(
+                    text = "Lorem ipsum dolor sit amet lorem ipsum dolor sit amet",
+                    style = MaterialTheme.typography.titleSmall,
+                    color = MaterialTheme.colorScheme.onBackground,
+                    overflow = TextOverflow.Ellipsis,
+                    maxLines = 1,
+                )
+                Text(
+                    text = "Lorem ipsum dolor sit",
+                    style = MaterialTheme.typography.bodySmall,
+                    color = MaterialTheme.colorScheme.onSurface,
+                )
+            }
+
+            Spacer(modifier = Modifier.width(8.dp))
+
+            Text(
+                text = "11.45",
+                style = MaterialTheme.typography.bodySmall,
+                color = MaterialTheme.colorScheme.onSurface,
+            )
+        }
+    }
+}
+
+@Composable
+private fun getNotificationIcon(type: NotificationType) = when (type) {
+    NotificationType.MODULE -> painterResource(id = designSystemR.drawable.ic_book)
+    NotificationType.ASSIGNMENT -> painterResource(id = designSystemR.drawable.ic_assignment)
+    NotificationType.QUIZ -> painterResource(id = designSystemR.drawable.ic_quiz)
+}
+
+enum class NotificationType {
+    MODULE,
+    ASSIGNMENT,
+    QUIZ,
 }
