b99ab755d8e421d4e18fa6c14171360858c31066
muammarahlnn <21muammarahlan@gmail.com>
2023-08-08 01:21:25 +0800

    feat(core-ui): add ClassResourceCard

---
+++ core/ui/src/main/java/com/muammarahlnn/learnyscape/core/ui/ClassResourceCard.kt
@@ -0,0 +1,104 @@
+package com.muammarahlnn.learnyscape.core.ui
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import com.muammarahlnn.learnyscape.core.model.ClassResourceType
+import com.muammarahlnn.learnyscape.core.designsystem.R as designSystemR
+
+
+/**
+ * @author Muammar Ahlan Abimanyu (muammarahlnn)
+ * @file ClassResourceCard, 07/08/2023 22.39 by Muammar Ahlan Abimanyu
+ */
+
+@Composable
+fun ClassResourceCard(
+    classResourceType: ClassResourceType,
+    title: String,
+    timeLabel: String,
+    modifier: Modifier = Modifier,
+    onItemClick: () -> Unit = {},
+) {
+    Card(
+        shape = RoundedCornerShape(10.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.onPrimary,
+        ),
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 2.dp,
+        ),
+        modifier = modifier
+            .fillMaxWidth()
+            .clickable {
+                onItemClick()
+            }
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Box(
+                contentAlignment = Alignment.Center,
+                modifier = Modifier
+                    .clip(CircleShape)
+                    .background(MaterialTheme.colorScheme.primary)
+                    .padding(10.dp)
+            ) {
+                Image(
+                    painter = getClassResourceIcon(type = classResourceType),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(28.dp)
+                )
+            }
+            Spacer(modifier = Modifier.width(12.dp))
+            Column {
+                // this is a hardcoded text just for dummy purpose
+                Text(
+                    text = title,
+                    color = MaterialTheme.colorScheme.onBackground,
+                    style = MaterialTheme.typography.titleSmall.copy(
+                        fontWeight = FontWeight.SemiBold
+                    )
+                )
+                Text(
+                    text = timeLabel,
+                    color = MaterialTheme.colorScheme.onSurface,
+                    style = MaterialTheme.typography.bodySmall,
+                    overflow = TextOverflow.Ellipsis,
+                    maxLines = 1,
+                )
+            }
+        }
+    }
+}
+
+@Composable
+private fun getClassResourceIcon(type: ClassResourceType) = when (type) {
+    ClassResourceType.MODULE -> painterResource(id = designSystemR.drawable.ic_book)
+    ClassResourceType.ASSIGNMENT -> painterResource(id = designSystemR.drawable.ic_assignment)
+    ClassResourceType.QUIZ -> painterResource(id = designSystemR.drawable.ic_quiz)
+}
