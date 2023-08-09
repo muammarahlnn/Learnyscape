fd646bf7a8ca1ef209f785633e15d4e24acb95ea
muammarahlnn <21muammarahlan@gmail.com>
2023-08-09 19:54:06 +0800

    feat(class): add ClassHeader

--- feature/class/src/main/java/com/muammarahlnn/learnyscape/feature/aclass/ClassScreen.kt
+++ feature/class/src/main/java/com/muammarahlnn/learnyscape/feature/aclass/ClassScreen.kt
@@ -1,15 +1,19 @@
 package com.muammarahlnn.learnyscape.feature.aclass
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import com.muammarahlnn.learnyscape.core.ui.ClassTopAppBar
 
 
@@ -37,14 +41,29 @@ private fun ClassScreen(
 ) {
     Column(modifier = modifier) {
         ClassTopAppBar(onBackClick = onBackClick)
-        Box(
-            contentAlignment = Alignment.Center,
-            modifier = modifier.fillMaxSize()
-        ) {
-            Text(
-                text = stringResource(id = R.string.announcement),
-                color = MaterialTheme.colorScheme.onBackground
-            )
-        }
+
+        ClassHeader()
+    }
+}
+
+@Composable
+private fun ClassHeader() {
+    Box {
+        Image(
+            painter = painterResource(id = R.drawable.bg_class_header_gradient),
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(160.dp)
+        )
+
+        Image(
+            painter = painterResource(id = R.drawable.ic_groups),
+            contentDescription = stringResource(id = R.string.groups),
+            modifier = Modifier
+                .size(120.dp)
+                .align(Alignment.TopCenter)
+        )
     }
 }
