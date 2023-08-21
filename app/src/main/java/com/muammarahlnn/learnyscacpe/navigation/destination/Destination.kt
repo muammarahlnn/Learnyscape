58a3b291393f445de53b2f15be9f2ac4e908d086
muammarahlnn <21muammarahlan@gmail.com>
2023-08-21 18:51:24 +0800

    fix(app): fix double selected class bottom navigation item

--- app/src/main/java/com/muammarahlnn/learnyscacpe/navigation/destination/Destination.kt
+++ app/src/main/java/com/muammarahlnn/learnyscacpe/navigation/destination/Destination.kt
@@ -1,16 +1,24 @@
 package com.muammarahlnn.learnyscacpe.navigation.destination
 
 import androidx.annotation.DrawableRes
-import androidx.annotation.StringRes
+import com.muammarahlnn.learnyscape.feature.aclass.navigation.classRoute
+import com.muammarahlnn.learnyscape.feature.assignment.navigation.assignmentRoute
+import com.muammarahlnn.learnyscape.feature.home.navigation.homeRoute
+import com.muammarahlnn.learnyscape.feature.member.navigation.memberRoute
+import com.muammarahlnn.learnyscape.feature.module.navigation.moduleRoute
+import com.muammarahlnn.learnyscape.feature.profile.navigation.profileRoute
+import com.muammarahlnn.learnyscape.feature.quiz.navigation.quizRoute
+import com.muammarahlnn.learnyscape.feature.schedule.navigation.scheduleRoute
+import com.muammarahlnn.learnyscape.feature.search.navigation.searchRoute
+import com.muammarahlnn.learnyscape.feature.aclass.R as classR
+import com.muammarahlnn.learnyscape.feature.assignment.R as assignmentR
 import com.muammarahlnn.learnyscape.feature.home.R as homeR
+import com.muammarahlnn.learnyscape.feature.member.R as memberR
+import com.muammarahlnn.learnyscape.feature.module.R as moduleR
 import com.muammarahlnn.learnyscape.feature.profile.R as profileR
+import com.muammarahlnn.learnyscape.feature.quiz.R as quizR
 import com.muammarahlnn.learnyscape.feature.schedule.R as scheduleR
 import com.muammarahlnn.learnyscape.feature.search.R as searchR
-import com.muammarahlnn.learnyscape.feature.aclass.R as classR
-import com.muammarahlnn.learnyscape.feature.module.R as moduleR
-import com.muammarahlnn.learnyscape.feature.assignment.R as assignmentR
-import com.muammarahlnn.learnyscape.feature.quiz.R as quizR
-import com.muammarahlnn.learnyscape.feature.member.R as memberR
 
 
 /**
@@ -20,8 +28,7 @@ import com.muammarahlnn.learnyscape.feature.member.R as memberR
 
 sealed interface Destination {
 
-    @get:StringRes
-    val nameId: Int
+    val route: String
 
     @get:DrawableRes
     val selectedIconId: Int
@@ -30,61 +37,61 @@ sealed interface Destination {
     val unselectedIconId: Int
 
     enum class TopLevelDestination(
-        override val nameId: Int,
+        override val route: String,
         override val selectedIconId: Int,
         override val unselectedIconId: Int,
     ) : Destination {
 
         HOME(
-            nameId = homeR.string.home,
+            route = homeRoute,
             selectedIconId = homeR.drawable.ic_home,
             unselectedIconId = homeR.drawable.ic_home_border,
         ),
         SEARCH(
-            nameId = searchR.string.search,
+            route = searchRoute,
             selectedIconId = searchR.drawable.ic_search,
             unselectedIconId = searchR.drawable.ic_search,
         ),
         SCHEDULE(
-            nameId = scheduleR.string.schedule,
+            route = scheduleRoute,
             selectedIconId = scheduleR.drawable.ic_calendar_today,
             unselectedIconId = scheduleR.drawable.ic_calendary_today_border,
         ),
         PROFILE(
-            nameId = profileR.string.profile,
+            route = profileRoute,
             selectedIconId = profileR.drawable.ic_person,
             unselectedIconId = profileR.drawable.ic_person_border,
         ),
     }
 
     enum class ClassDestination(
-        override val nameId: Int,
+        override val route: String,
         override val selectedIconId: Int,
         override val unselectedIconId: Int,
     ) : Destination {
 
         CLASS(
-            nameId = classR.string.announcement,
+            route = classRoute,
             selectedIconId = classR.drawable.ic_announcement,
             unselectedIconId = classR.drawable.ic_announcement_border,
         ),
         MODULE(
-            nameId = moduleR.string.module,
+            route = moduleRoute,
             selectedIconId = moduleR.drawable.ic_book,
             unselectedIconId = moduleR.drawable.ic_book_border,
         ),
         ASSIGNMENT(
-            nameId = assignmentR.string.assignment,
+            route = assignmentRoute,
             selectedIconId = assignmentR.drawable.ic_assignment,
             unselectedIconId = assignmentR.drawable.ic_assignment_border,
         ),
         QUIZ(
-            nameId = quizR.string.quiz,
+            route = quizRoute,
             selectedIconId = quizR.drawable.ic_quiz,
             unselectedIconId = quizR.drawable.ic_quiz_border,
         ),
         MEMBER(
-            nameId = memberR.string.member,
+            route = memberRoute,
             selectedIconId = memberR.drawable.ic_group,
             unselectedIconId = memberR.drawable.ic_group_border,
         ),
