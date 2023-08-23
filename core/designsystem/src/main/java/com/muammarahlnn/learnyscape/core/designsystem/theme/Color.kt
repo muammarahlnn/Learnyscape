760121366e9a4c724d35c7b4e3b1478670f324fb
muammarahlnn <21muammarahlan@gmail.com>
2023-08-23 18:17:25 +0800

    feat(resourcedetails): add ResourceDetailsContent

--- core/designsystem/src/main/java/com/muammarahlnn/learnyscape/core/designsystem/theme/Color.kt
+++ core/designsystem/src/main/java/com/muammarahlnn/learnyscape/core/designsystem/theme/Color.kt
@@ -10,6 +10,7 @@ import androidx.compose.ui.graphics.Color
 
 
 internal val White = Color(0xFFFFFFFF)
+internal val MediumWhite = Color(0xFFF1F1F1)
 internal val WhiteSmoke = Color(0xFFF5F5F5)
 internal val Red = Color(0xFFDF2E38)
 internal val LightRed = Color(0xFFEF6262)
@@ -17,3 +18,4 @@ internal val Blue = Color(0xFF0079FF)
 internal val Black = Color(0xFF2D2727)
 internal val Gray = Color(0xFF6A6A6A)
 internal val LightGray = Color(0xFFAAAAAA)
+
