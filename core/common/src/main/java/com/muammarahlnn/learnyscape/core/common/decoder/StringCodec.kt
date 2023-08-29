841ee5b8650420d39b533b536943fe4ee728e229
muammarahlnn <21muammarahlan@gmail.com>
2023-08-29 11:31:36 +0800

    feat(core-common): add StringCodec

---
+++ core/common/src/main/java/com/muammarahlnn/learnyscape/core/common/decoder/StringCodec.kt
@@ -0,0 +1,19 @@
+package com.muammarahlnn.learnyscape.core.common.decoder
+
+import java.net.URLDecoder
+import java.net.URLEncoder
+import kotlin.text.Charsets.UTF_8
+
+
+/**
+ * @author Muammar Ahlan Abimanyu (muammarahlnn)
+ * @file StringCodec, 29/08/2023 11.21 by Muammar Ahlan Abimanyu
+ */
+object StringCodec {
+
+    private val urlCharacterEncoding = UTF_8.name()
+
+    fun encodeString(string: String): String = URLEncoder.encode(string, urlCharacterEncoding)
+
+    fun decodeString(string: String) : String = URLDecoder.decode(string, urlCharacterEncoding)
+}
