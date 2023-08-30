66d8a5ee1e5aaeee415ac64b7bb1df3f75afadc3
muammarahlnn <21muammarahlan@gmail.com>
2023-08-31 02:59:10 +0800

    feat(splash): add Splash Screen using SplashScreen API

--- app/src/main/java/com/muammarahlnn/learnyscape/MainActivity.kt
+++ app/src/main/java/com/muammarahlnn/learnyscape/MainActivity.kt
@@ -3,6 +3,7 @@ package com.muammarahlnn.learnyscape
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.muammarahlnn.learnyscape.core.designsystem.theme.LearnyscapeTheme
 import com.muammarahlnn.learnyscape.ui.LearnyscapeApp
 import dagger.hilt.android.AndroidEntryPoint
@@ -11,7 +12,9 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
+
         setContent {
             LearnyscapeTheme {
                 LearnyscapeApp()
