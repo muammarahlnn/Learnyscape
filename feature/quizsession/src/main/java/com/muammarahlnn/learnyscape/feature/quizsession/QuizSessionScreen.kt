de5c5a15c8b809e73e54faed651f97473fc94889
muammarahlnn <21muammarahlan@gmail.com>
2023-09-01 02:05:40 +0800

    feat(quizsession): add QuizSessionContent

--- feature/quizsession/src/main/java/com/muammarahlnn/learnyscape/feature/quizsession/QuizSessionScreen.kt
+++ feature/quizsession/src/main/java/com/muammarahlnn/learnyscape/feature/quizsession/QuizSessionScreen.kt
@@ -1,13 +1,34 @@
 package com.muammarahlnn.learnyscape.feature.quizsession
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 
 
@@ -34,18 +55,150 @@ private fun QuizSessionScreen(
     quizDuration: Int,
     modifier: Modifier = Modifier,
 ) {
-    Column(modifier = modifier) {
+    Column(modifier = modifier.fillMaxSize()) {
         QuizSessionTopAppBar(
             quizName = quizName,
             quizDuration = quizDuration,
         )
-        Box(
-            modifier = Modifier.fillMaxSize()
-        ) {
-            Text(
-                text = stringResource(id = R.string.quiz),
-                modifier = Modifier.align(Alignment.Center)
+        QuizSessionContent()
+    }
+}
+
+@Composable
+private fun QuizSessionContent(
+    modifier: Modifier = Modifier,
+) {
+    val questions = listOf(
+        stringResource(id = R.string.dummy_question),
+        stringResource(id = R.string.dummy_question),
+        stringResource(id = R.string.dummy_question),
+        stringResource(id = R.string.dummy_question),
+        stringResource(id = R.string.dummy_question),
+    )
+    LazyColumn(
+        contentPadding = PaddingValues(16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp),
+        modifier = modifier,
+    ) {
+        itemsIndexed(questions) { index, item ->
+            QuestionItem(
+                number = index + 1,
+                question = item
             )
         }
+    }
+}
+
+@Composable
+private fun QuestionItem(
+    number: Int,
+    question: String,
+    modifier: Modifier = Modifier,
+) {
+    Row(modifier = modifier) {
+        QuestionNumber(number = number)
+        
+        Spacer(modifier = Modifier.width(10.dp))
+
+        Column {
+            QuestionText(question = question)
+            
+            Spacer(modifier = Modifier.height(10.dp))
+
+            QuestionMultipleChoice()
+        }
+    }
+}
+
+@Composable
+private fun QuestionNumber(number: Int) {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier
+            .shadow(
+                elevation = 2.dp,
+                shape = CircleShape,
+            )
+            .background(MaterialTheme.colorScheme.onPrimary)
+            .clip(CircleShape)
+            .size(30.dp)
+            .padding(5.dp)
+    ) {
+        Text(
+            text = "$number",
+            style = MaterialTheme.typography.labelMedium.copy(
+                fontWeight = FontWeight.SemiBold
+            ),
+            color = MaterialTheme.colorScheme.onBackground,
+        )
+    }
+}
+
+@Composable
+private fun QuestionText(
+    question: String,
+) {
+    Card(
+        shape = RoundedCornerShape(10.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.onPrimary,
+        ),
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 2.dp,
+        ),
+        modifier = Modifier
+            .fillMaxWidth()
+    ) {
+        Column(modifier = Modifier.padding(10.dp)) {
+            Text(
+                text = question,
+                style = MaterialTheme.typography.bodySmall,
+                color = MaterialTheme.colorScheme.onBackground,
+            )
+        }
+    }
+}
+
+@Composable
+private fun QuestionMultipleChoice() {
+    val choices = listOf("A", "B", "C", "D", "E")
+    choices.forEachIndexed { index, choice ->
+        Card(
+            shape = RoundedCornerShape(10.dp),
+            colors = CardDefaults.cardColors(
+                containerColor = MaterialTheme.colorScheme.onPrimary,
+            ),
+            elevation = CardDefaults.cardElevation(
+                defaultElevation = 2.dp,
+            ),
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Row(
+                modifier = Modifier.padding(10.dp)
+            ) {
+                Text(
+                    text = "${choice}. ",
+                    style = MaterialTheme.typography.bodySmall.copy(
+                        fontWeight = FontWeight.SemiBold
+                    ),
+                    color = MaterialTheme.colorScheme.onBackground,
+                )
+
+                Spacer(modifier = Modifier.width(2.dp))
+
+                Text(
+                    text = if (index % 2 == 0) {
+                        "Lorem ipsum dolor sit amet. Est rerum fugit sed quia rerum qui nihil asperiores aut mollitia numquam"
+                    } else {
+                        "Lorem ipsum dolor sit amet"
+                    },
+                    style = MaterialTheme.typography.bodySmall,
+                    color = MaterialTheme.colorScheme.onBackground,
+                    modifier = Modifier.align(Alignment.CenterVertically)
+                )
+            }
+        }
+
+        Spacer(modifier = Modifier.height(5.dp))
     }
 }
