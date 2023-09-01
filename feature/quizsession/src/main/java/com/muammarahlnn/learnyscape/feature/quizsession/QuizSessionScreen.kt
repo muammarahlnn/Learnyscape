a2469ded16e87afe452605c0fec0b062ec2702c6
muammarahlnn <21muammarahlan@gmail.com>
2023-09-02 02:02:38 +0800

    feat(quizsession): make questions' options selectable

--- feature/quizsession/src/main/java/com/muammarahlnn/learnyscape/feature/quizsession/QuizSessionScreen.kt
+++ feature/quizsession/src/main/java/com/muammarahlnn/learnyscape/feature/quizsession/QuizSessionScreen.kt
@@ -1,6 +1,7 @@
 package com.muammarahlnn.learnyscape.feature.quizsession
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -22,6 +23,8 @@ import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.toMutableStateList
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -68,22 +71,32 @@ private fun QuizSessionScreen(
 private fun QuizSessionContent(
     modifier: Modifier = Modifier,
 ) {
-    val questions = listOf(
-        stringResource(id = R.string.dummy_question),
-        stringResource(id = R.string.dummy_question),
-        stringResource(id = R.string.dummy_question),
-        stringResource(id = R.string.dummy_question),
-        stringResource(id = R.string.dummy_question),
-    )
+    val questions = generateDummyQuestions()
+    // TODO: Save it to View Model so it will survive the activity or process recreation
+    val selectedOptionLetters = remember {
+        List(questions.size) {
+            OptionLetter.UNSELECTED
+        }.toMutableStateList()
+    }
+
     LazyColumn(
         contentPadding = PaddingValues(16.dp),
         verticalArrangement = Arrangement.spacedBy(16.dp),
         modifier = modifier,
     ) {
-        itemsIndexed(questions) { index, item ->
+        itemsIndexed(
+            items = questions,
+            key = { _, item ->
+                item.id
+            }
+        ) { index, question ->
             QuestionItem(
                 number = index + 1,
-                question = item
+                question = question,
+                selectedOptionLetter = selectedOptionLetters[index],
+                onOptionSelect = { optionLetter ->
+                    selectedOptionLetters[index] = optionLetter
+                }
             )
         }
     }
@@ -92,20 +105,26 @@ private fun QuizSessionContent(
 @Composable
 private fun QuestionItem(
     number: Int,
-    question: String,
+    question: MultipleChoiceQuestion,
+    selectedOptionLetter: OptionLetter,
+    onOptionSelect: (OptionLetter) -> Unit,
     modifier: Modifier = Modifier,
 ) {
     Row(modifier = modifier) {
         QuestionNumber(number = number)
-        
+
         Spacer(modifier = Modifier.width(10.dp))
 
         Column {
-            QuestionText(question = question)
+            QuestionText(question = question.question)
             
             Spacer(modifier = Modifier.height(10.dp))
 
-            QuestionMultipleChoice()
+            QuestionMultipleChoice(
+                options = question.options,
+                currentSelectedOptionLetter = selectedOptionLetter,
+                onOptionSelect = onOptionSelect
+            )
         }
     }
 }
@@ -160,40 +179,51 @@ private fun QuestionText(
 }
 
 @Composable
-private fun QuestionMultipleChoice() {
-    val choices = listOf("A", "B", "C", "D", "E")
-    choices.forEachIndexed { index, choice ->
+private fun QuestionMultipleChoice(
+    options: List<Option>,
+    currentSelectedOptionLetter: OptionLetter,
+    onOptionSelect: (OptionLetter) -> Unit,
+) {
+    options.forEach { option ->
+        val isSelected = currentSelectedOptionLetter == option.letter
+        // TODO: Animated this colors
+        val (backgroundColor, textColor) = if (isSelected) {
+            MaterialTheme.colorScheme.primary to MaterialTheme.colorScheme.onPrimary
+        } else {
+            MaterialTheme.colorScheme.onPrimary to MaterialTheme.colorScheme.onBackground
+        }
+
         Card(
             shape = RoundedCornerShape(10.dp),
             colors = CardDefaults.cardColors(
-                containerColor = MaterialTheme.colorScheme.onPrimary,
+                containerColor = backgroundColor,
             ),
             elevation = CardDefaults.cardElevation(
                 defaultElevation = 2.dp,
             ),
-            modifier = Modifier.fillMaxWidth()
+            modifier = Modifier
+                .fillMaxWidth()
+                .clickable {
+                    onOptionSelect(option.letter)
+                }
         ) {
             Row(
                 modifier = Modifier.padding(10.dp)
             ) {
                 Text(
-                    text = "${choice}. ",
+                    text = "${option.letter.name}. ",
                     style = MaterialTheme.typography.bodySmall.copy(
                         fontWeight = FontWeight.SemiBold
                     ),
-                    color = MaterialTheme.colorScheme.onBackground,
+                    color = textColor,
                 )
 
                 Spacer(modifier = Modifier.width(2.dp))
 
                 Text(
-                    text = if (index % 2 == 0) {
-                        "Lorem ipsum dolor sit amet. Est rerum fugit sed quia rerum qui nihil asperiores aut mollitia numquam"
-                    } else {
-                        "Lorem ipsum dolor sit amet"
-                    },
+                    text = option.text,
                     style = MaterialTheme.typography.bodySmall,
-                    color = MaterialTheme.colorScheme.onBackground,
+                    color = textColor,
                     modifier = Modifier.align(Alignment.CenterVertically)
                 )
             }
@@ -202,3 +232,36 @@ private fun QuestionMultipleChoice() {
         Spacer(modifier = Modifier.height(5.dp))
     }
 }
+
+data class MultipleChoiceQuestion(
+    val id: Int,
+    val question: String,
+    val options: List<Option>
+)
+
+data class Option(
+    val letter: OptionLetter,
+    val text: String
+)
+
+enum class OptionLetter {
+    A, B, C, D, E,
+    UNSELECTED,
+}
+
+@Composable
+fun generateDummyQuestions(): List<MultipleChoiceQuestion> =
+    List(10) { index ->
+        MultipleChoiceQuestion(
+            id = index,
+            question = stringResource(id = R.string.dummy_question),
+            options = listOf(
+                Option(OptionLetter.A, "Lorem Ipsum Dolor Sit Amet"),
+                Option(OptionLetter.B, "Lorem ipsum dolor sit amet. Est rerum fugit sed quia rerum qui nihil asperiores aut mollitia numquam"),
+                Option(OptionLetter.C, "Lorem Ipsum Dolor Sit Amet"),
+                Option(OptionLetter.D, "Lorem ipsum dolor sit amet. Est rerum fugit sed quia rerum qui nihil asperiores aut mollitia numquam"),
+                Option(OptionLetter.E, "Lorem Ipsum Dolor Sit Amet"),
+            )
+        )
+    }
+
