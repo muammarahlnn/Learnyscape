3d31abc76da735041d882f8069bacdb2692ab984
muammarahlnn <21muammarahlan@gmail.com>
2023-09-06 15:09:22 +0800

    feat(resourcedetails): make quiz type information based on QuizType model

---
+++ core/model/src/main/java/com/muammarahlnn/learnyscape/core/model/QuizType.kt
@@ -0,0 +1,13 @@
+package com.muammarahlnn.learnyscape.core.model
+
+
+/**
+ * @author Muammar Ahlan Abimanyu (muammarahlnn)
+ * @file QuizType, 06/09/2023 01.29 by Muammar Ahlan Abimanyu
+ */
+
+// Use the enum ordinal to be an argument in navigation
+enum class QuizType {
+    MULTIPLE_CHOICE_QUESTIONS,
+    PHOTO_ANSWER,
+}
