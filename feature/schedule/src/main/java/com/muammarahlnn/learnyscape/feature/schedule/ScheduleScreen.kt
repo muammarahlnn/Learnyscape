b538c0670be88109c8183ffbb03f48b3d5c605b4
muammarahlnn <21muammarahlan@gmail.com>
2023-09-22 22:12:10 +0800

    feat(schedule): add TodayScheduleCalendar

--- feature/schedule/src/main/java/com/muammarahlnn/learnyscape/feature/schedule/ScheduleScreen.kt
+++ feature/schedule/src/main/java/com/muammarahlnn/learnyscape/feature/schedule/ScheduleScreen.kt
@@ -1,18 +1,29 @@
 package com.muammarahlnn.learnyscape.feature.schedule
 
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import com.muammarahlnn.learnyscape.core.designsystem.component.LearnyscapeTopAppBar
 import com.muammarahlnn.learnyscape.core.designsystem.component.defaultTopAppBarColors
+import kotlinx.datetime.LocalTime
 
 
 /**
@@ -25,14 +36,129 @@ import com.muammarahlnn.learnyscape.core.designsystem.component.defaultTopAppBar
 internal fun ScheduleScreen(modifier: Modifier = Modifier) {
     Column(modifier = modifier) {
         ScheduleTopAppBar()
-        Box(
-            contentAlignment = Alignment.Center,
-            modifier = modifier
-                .fillMaxSize()
+        ScheduleDateHeader()
+        ScheduleContent()
+    }
+}
+
+@Composable
+private fun ScheduleContent(
+    modifier: Modifier = Modifier,
+) {
+    LazyColumn(
+        contentPadding = PaddingValues(16.dp),
+        modifier = modifier.fillMaxSize(),
+    ) {
+        item {
+            TodayScheduleCalendar(
+                hourLabel = { hour ->
+                    HourLabel(hour = hour)
+                },
+                scheduleClass = { scheduleClass ->
+                    ScheduleClassCard(
+                        className = scheduleClass.className,
+                        startTime = scheduleClass.startTime,
+                        endTime = scheduleClass.endTime,
+                        modifier = Modifier.scheduleClassCard(
+                            startTime = scheduleClass.startTime,
+                            endTime = scheduleClass.endTime,
+                        )
+                    )
+                },
+                modifier = modifier.wrapContentSize()
+            )
+        }
+    }
+}
+
+@Composable
+private fun ScheduleDateHeader(
+    modifier: Modifier = Modifier,
+) {
+    Card(
+        shape = RoundedCornerShape(
+            bottomStart = 10.dp,
+            bottomEnd = 10.dp,
+        ),
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.onPrimary,
+        ),
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 2.dp,
+        ),
+        modifier = modifier
+            .fillMaxWidth()
+    ) {
+        Column(
+            modifier = Modifier.padding(16.dp)
         ) {
             Text(
-                text = stringResource(id = R.string.schedule),
-                color = MaterialTheme.colorScheme.onBackground
+                text = "Monday",
+                style = MaterialTheme.typography.titleMedium,
+                color = MaterialTheme.colorScheme.onBackground,
+            )
+            Text(
+                text = "18 September 2023",
+                style = MaterialTheme.typography.bodyMedium,
+                color = MaterialTheme.colorScheme.onSurface,
+            )
+        }
+    }
+}
+
+
+@Composable
+fun HourLabel(hour: Int) {
+    val formattedHour = String.format("%02d:00", hour)
+    Text(
+        text = formattedHour,
+        style = MaterialTheme.typography.labelLarge,
+        color = MaterialTheme.colorScheme.onBackground,
+        textAlign = TextAlign.End,
+        modifier = Modifier.height(100.dp)
+    )
+}
+
+@Composable
+private fun ScheduleClassCard(
+    className: String,
+    startTime: LocalTime,
+    endTime: LocalTime,
+    modifier: Modifier = Modifier,
+) {
+    Card(
+        shape = RoundedCornerShape(10.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.onPrimary,
+        ),
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 2.dp,
+        ),
+        modifier = modifier,
+    ) {
+        Column(
+            modifier = Modifier.padding(
+                vertical = 8.dp,
+                horizontal = 16.dp,
+            ),
+        ) {
+            Text(
+                text = className,
+                style = MaterialTheme.typography.titleSmall.copy(
+                    fontWeight = FontWeight.SemiBold
+                ),
+                color = MaterialTheme.colorScheme.onBackground,
+            )
+            val formatHour = "%02d:%02d"
+            val formattedStartTime = String.format(formatHour, startTime.hour, startTime.minute)
+            val formattedEndTime = String.format(formatHour, endTime.hour, endTime.minute)
+            Text(
+                text = stringResource(
+                    id = R.string.class_range_time,
+                    formattedStartTime, formattedEndTime,
+                ),
+                style = MaterialTheme.typography.labelMedium,
+                color = MaterialTheme.colorScheme.onSurface,
             )
         }
     }
