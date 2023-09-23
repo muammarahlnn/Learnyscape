6f9ecb67fc8b57ddd1807fdf6ba351932fc70724
muammarahlnn <21muammarahlan@gmail.com>
2023-09-24 01:26:29 +0800

    feat(login): add login module

---
+++ feature/login/build.gradle.kts
@@ -0,0 +1,13 @@
+plugins {
+    id("learnyscape.android.feature")
+    id("learnyscape.android.library.compose")
+}
+
+android {
+    namespace = "com.muammarahlnn.learnyscape.feature.login"
+}
+
+dependencies {
+
+    implementation(libs.androidx.activity.compose)
+}
