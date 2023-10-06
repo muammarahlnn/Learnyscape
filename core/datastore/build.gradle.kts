a89f033359aad4a943da60ecc49d07cd5fd1cb9f
muammarahlnn <21muammarahlan@gmail.com>
2023-10-06 21:03:49 +0800

    feat(datastore): add core-datastore module

---
+++ core/datastore/build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    id("learnyscape.android.library")
+    id("learnyscape.android.hilt")
+}
+
+android {
+    namespace = "com.muammarahlnn.learnyscape.core.datastore"
+    testOptions {
+        unitTests {
+            isIncludeAndroidResources = true
+            isReturnDefaultValues = true
+        }
+    }
+}
+
+dependencies {
+    implementation(project(":core:common"))
+    implementation(project(":core:model"))
+
+    implementation(libs.androidx.dataStore.core)
+    implementation(libs.kotlinx.coroutines.android)
+}
