04778ac41b7fd3020a4c01e98844cea2d3547a1e
muammarahlnn <21muammarahlan@gmail.com>
2023-10-06 19:58:03 +0800

    feat(network): handle when users not connected to the internet

---
+++ core/network/src/main/java/com/muammarahlnn/learnyscape/core/network/interceptor/NetworkConnectionInterceptor.kt
@@ -0,0 +1,60 @@
+package com.muammarahlnn.learnyscape.core.network.interceptor
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
+import okhttp3.Interceptor
+import okhttp3.Response
+import java.io.IOException
+
+
+/**
+ * @author Muammar Ahlan Abimanyu (muammarahlnn)
+ * @file NetworkConnectionInterceptor, 06/10/2023 18.40 by Muammar Ahlan Abimanyu
+ */
+class NetworkConnectionInterceptor(
+    private val context: Context
+) : Interceptor {
+
+    override fun intercept(chain: Interceptor.Chain): Response {
+        if (!isConnected()) {
+            throw NoConnectivityException
+        }
+
+        val builder = chain.request().newBuilder()
+        return chain.proceed(builder.build())
+    }
+
+    private fun isConnected(): Boolean {
+        var connected = false
+        val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            val networkCapabilities = connectivityManager.activeNetwork ?: return false
+            val activeNetwork = connectivityManager.getNetworkCapabilities(networkCapabilities) ?: return false
+            connected = when {
+                activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+                activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+                activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
+                else -> false
+            }
+        } else {
+            connectivityManager.activeNetworkInfo?.run {
+                connected = when(type) {
+                    ConnectivityManager.TYPE_WIFI -> true
+                    ConnectivityManager.TYPE_MOBILE -> true
+                    ConnectivityManager.TYPE_ETHERNET -> true
+                    else -> false
+                }
+            }
+        }
+
+        return connected
+    }
+}
+
+object NoConnectivityException : IOException() {
+
+    override val message: String
+        get() = "No internet connection"
+}
