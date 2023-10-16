6558932893df1ceb9640a5cad499980b8be72e61
muammarahlnn <21muammarahlan@gmail.com>
2023-10-16 17:08:46 +0800

    cleanup(data): make mapping from NetworkResult to Result not boilerplate

--- core/data/src/main/java/com/muammarahlnn/learnyscape/core/data/repository/impl/HomeRepositoryImpl.kt
+++ core/data/src/main/java/com/muammarahlnn/learnyscape/core/data/repository/impl/HomeRepositoryImpl.kt
@@ -2,16 +2,15 @@ package com.muammarahlnn.learnyscape.core.data.repository.impl
 
 import com.muammarahlnn.learnyscape.core.common.result.Result
 import com.muammarahlnn.learnyscape.core.data.mapper.toClassesInfoModel
+import com.muammarahlnn.learnyscape.core.data.mapper.toResult
 import com.muammarahlnn.learnyscape.core.data.mapper.toUserModel
 import com.muammarahlnn.learnyscape.core.data.repository.HomeRepository
 import com.muammarahlnn.learnyscape.core.datastore.LearnyscapePreferencesDataSource
 import com.muammarahlnn.learnyscape.core.model.data.ClassInfoModel
 import com.muammarahlnn.learnyscape.core.model.data.UserModel
 import com.muammarahlnn.learnyscape.core.network.datasource.HomeNetworkDataSource
-import com.muammarahlnn.learnyscape.core.network.model.response.base.NetworkResult
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.onStart
 import javax.inject.Inject
 
 
@@ -30,20 +29,7 @@ class HomeRepositoryImpl @Inject constructor(
         }
 
     override fun getClasses(): Flow<Result<List<ClassInfoModel>>> =
-        homeNetworkDataSource.getClasses().map { networkResult ->
-            when (networkResult) {
-                is NetworkResult.Success -> Result.Success(
-                    data = networkResult.data.toClassesInfoModel()
-                )
-                is NetworkResult.Error -> Result.Error(
-                    code = networkResult.code,
-                    message = networkResult.message,
-                )
-                is NetworkResult.Exception -> Result.Exception(
-                    exception = networkResult.exception,
-                )
-            }
-        }.onStart {
-            emit(Result.Loading)
+        homeNetworkDataSource.getClasses().toResult { classesInfoResponse ->
+            classesInfoResponse.toClassesInfoModel()
         }
 }
