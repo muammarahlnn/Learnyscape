807d3ec41c6b2467f1427262a8975716186c959e
muammarahlnn <21muammarahlan@gmail.com>
2023-11-13 18:44:31 +0800

    feat(profile): add vertical scroll

--- feature/profile/src/main/java/com/muammarahlnn/learnyscape/feature/profile/ProfileScreen.kt
+++ feature/profile/src/main/java/com/muammarahlnn/learnyscape/feature/profile/ProfileScreen.kt
@@ -8,13 +8,16 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FilledIconButton
 import androidx.compose.material3.Icon
@@ -126,6 +129,8 @@ private fun ProfileScreen(
 
     Column(
         modifier = modifier
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState())
             .nestedScroll(scrollBehavior.nestedScrollConnection)
             .padding(16.dp),
     ) {
