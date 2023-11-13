9aab212e05ec4af4dc5dd453ec83750786afb1ea
muammarahlnn <21muammarahlan@gmail.com>
2023-11-13 19:51:07 +0800

    feat(home): add ShimmerLoadingEffect and ErrorScreen

--- core/ui/src/main/java/com/muammarahlnn/learnyscape/core/ui/util/UiHelper.kt
+++ core/ui/src/main/java/com/muammarahlnn/learnyscape/core/ui/util/UiHelper.kt
@@ -1,6 +1,22 @@
 package com.muammarahlnn.learnyscape.core.ui.util
 
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.background
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.runtime.staticCompositionLocalOf
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.unit.IntSize
 import com.muammarahlnn.learnyscape.core.model.data.UserModel
 
 
@@ -10,3 +26,32 @@ import com.muammarahlnn.learnyscape.core.model.data.UserModel
  */
 
 val LocalUserModel = staticCompositionLocalOf { UserModel() }
+
+fun Modifier.shimmerEffect(): Modifier = composed {
+    var size by remember { mutableStateOf(IntSize.Zero) }
+    val transition = rememberInfiniteTransition(
+        label = "Shimmer infinite transition"
+    )
+    val startOffsetX by transition.animateFloat(
+        initialValue = -1.2f * size.width.toFloat(),
+        targetValue = 1.2f * size.width.toFloat(),
+        animationSpec = infiniteRepeatable(
+            animation = tween(1500)
+        ),
+        label = "Shimmer start offset X animation"
+    )
+
+    background(
+        brush = Brush.linearGradient(
+            colors = listOf(
+                MaterialTheme.colorScheme.onSecondary,
+                MaterialTheme.colorScheme.surfaceVariant,
+                MaterialTheme.colorScheme.onSecondary,
+            ),
+            start = Offset(startOffsetX, 0f),
+            end = Offset(startOffsetX + size.width.toFloat(), size.height.toFloat())
+        )
+    ).onGloballyPositioned { layoutCoordinates ->
+        size = layoutCoordinates.size
+    }
+}
