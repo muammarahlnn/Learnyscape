14cc5b9e5f93b4c26751d3fe2b4d8394689668e8
muammarahlnn <21muammarahlan@gmail.com>
2023-12-12 22:13:00 +0800

    feat(home): implement MVI

--- feature/home/src/main/java/com/muammarahlnn/learnyscape/feature/home/HomeViewModel.kt
+++ feature/home/src/main/java/com/muammarahlnn/learnyscape/feature/home/HomeViewModel.kt
@@ -1,7 +1,6 @@
 package com.muammarahlnn.learnyscape.feature.home
 
 import android.util.Log
-import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.muammarahlnn.learnyscape.core.common.result.asResult
@@ -13,7 +12,7 @@ import com.muammarahlnn.learnyscape.core.common.result.onSuccess
 import com.muammarahlnn.learnyscape.core.domain.home.GetEnrolledClassesUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -25,60 +24,64 @@ import javax.inject.Inject
  */
 @HiltViewModel
 class HomeViewModel @Inject constructor(
-    private val savedStateHandle: SavedStateHandle,
     private val getEnrolledClassesUseCase: GetEnrolledClassesUseCase
-) : ViewModel() {
+) : ViewModel(), HomeContract {
 
-    val searchQuery = savedStateHandle.getStateFlow(key = SEARCH_QUERY, initialValue = "")
+    private val _state = MutableStateFlow(HomeContract.State())
+    override val state: StateFlow<HomeContract.State> = _state
 
-    private val _homeUiState = MutableStateFlow<HomeUiState>(HomeUiState.Loading)
+    private val _refreshing = MutableStateFlow(false)
+    override val refreshing: StateFlow<Boolean> = _refreshing
 
-    val homeUiState = _homeUiState.asStateFlow()
-
-    private val _isRefreshing = MutableStateFlow(false)
-
-    val isRefreshing = _isRefreshing.asStateFlow()
-
-    init {
-        fetchClasses()
+    override fun event(event: HomeContract.Event) = when (event) {
+        HomeContract.Event.FetchEnrolledClasses -> fetchEnrolledClasses()
+        is HomeContract.Event.OnSearchQueryChanged -> onSearchQueryChanged(event.query)
     }
 
-    fun fetchClasses() {
+    private fun fetchEnrolledClasses() {
         viewModelScope.launch {
             getEnrolledClassesUseCase().asResult().collect { result ->
                 result.onLoading {
-                    _homeUiState.update {
-                        HomeUiState.Loading
+                    _state.update {
+                        it.copy(uiState = HomeContract.UiState.Loading)
                     }
-                }.onSuccess { classInfos ->
-                    _homeUiState.update {
-                        if (classInfos.isNotEmpty()) {
-                            HomeUiState.Success(classInfos)
-                        } else {
-                            HomeUiState.SuccessEmptyClasses
-                        }
+                }.onSuccess { enrolledClasses ->
+                    _state.update {
+                        it.copy(
+                            uiState = if (enrolledClasses.isNotEmpty()) {
+                                HomeContract.UiState.Success(enrolledClasses)
+                            } else {
+                                HomeContract.UiState.SuccessEmptyClasses
+                            }
+                        )
                     }
                 }.onNoInternet { message ->
-                    _homeUiState.update {
-                        HomeUiState.NoInternet(message)
+                    _state.update {
+                        it.copy(uiState = HomeContract.UiState.NoInternet(message))
                     }
                 }.onError { _, message ->
-                    _homeUiState.update {
-                        HomeUiState.Error(message)
+                    Log.e(TAG, message)
+                    _state.update {
+                        it.copy(uiState = HomeContract.UiState.Error(message))
                     }
                 }.onException { exception, message ->
-                    Log.e("HomeViewModel", exception?.message.toString())
-                    _homeUiState.update {
-                        HomeUiState.Error(message)
+                    Log.e(TAG, exception?.message.toString())
+                    _state.update {
+                        it.copy(uiState = HomeContract.UiState.Error(message))
                     }
                 }
             }
         }
     }
 
-    fun onSearchQueryChanged(query: String) {
-        savedStateHandle[SEARCH_QUERY] = query
+    private fun onSearchQueryChanged(query: String) {
+        _state.update {
+            it.copy(searchQuery = query)
+        }
+    }
+
+    companion object {
+
+        private const val TAG = "HomeViewModel"
     }
 }
-
-private const val SEARCH_QUERY = "search_query"
