8248cf851183d0e8bd8ef9537699d86a993457e1
muammarahlnn <21muammarahlan@gmail.com>
2023-12-25 04:18:39 +0800

    refactor(resourcecreate): tighten MVI pattern

--- feature/resourcecreate/src/main/java/com/muammarahlnn/learnyscape/feature/resourcecreate/ResourceCreateViewModel.kt
+++ feature/resourcecreate/src/main/java/com/muammarahlnn/learnyscape/feature/resourcecreate/ResourceCreateViewModel.kt
@@ -1,9 +1,15 @@
 package com.muammarahlnn.learnyscape.feature.resourcecreate
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import java.io.File
 
 /**
@@ -13,16 +19,39 @@ import java.io.File
 class ResourceCreateViewModel : ViewModel(), ResourceCreateContract {
 
     private val _state = MutableStateFlow(ResourceCreateContract.State())
-    override val state: StateFlow<ResourceCreateContract.State> = _state
+    override val state: StateFlow<ResourceCreateContract.State> = _state.asStateFlow()
+
+    private val _effect = MutableSharedFlow<ResourceCreateContract.Effect>()
+    override val effect: SharedFlow<ResourceCreateContract.Effect> = _effect.asSharedFlow()
 
     override fun event(event: ResourceCreateContract.Event) {
         when (event) {
+            ResourceCreateContract.Event.OnCloseClick ->
+                closeScreen()
+
             is ResourceCreateContract.Event.OnDescriptionChange ->
                 onDescriptionChange(event.description)
-            is ResourceCreateContract.Event.OnShowUploadAttachmentBottomSheet  ->
-                showUploadAttachmentBottomSheet(event.show)
-            is ResourceCreateContract.Event.OnUploadFile ->
-                onUploadFile(event.selectedFile)
+
+            ResourceCreateContract.Event.OnAddAttachmentClick ->
+                showUploadAttachmentBottomSheet(true)
+
+            ResourceCreateContract.Event.OnCameraClick ->
+                openCamera()
+
+            ResourceCreateContract.Event.OnUploadFileClick ->
+                openFiles()
+
+            ResourceCreateContract.Event.OnDismissUploadAttachmentBottomSheet ->
+                showUploadAttachmentBottomSheet(false)
+
+            is ResourceCreateContract.Event.OnFileSelected ->
+                addAttachmentToCurrentAttachments(event.selectedFile)
+        }
+    }
+
+    private fun closeScreen() {
+        viewModelScope.launch {
+            _effect.emit(ResourceCreateContract.Effect.CloseScreen)
         }
     }
 
@@ -38,7 +67,21 @@ class ResourceCreateViewModel : ViewModel(), ResourceCreateContract {
         }
     }
 
-    private fun onUploadFile(file: File) {
+    private fun openCamera() {
+        viewModelScope.launch {
+            _effect.emit(ResourceCreateContract.Effect.OpenCamera)
+        }
+        showUploadAttachmentBottomSheet(false)
+    }
+
+    private fun openFiles() {
+        viewModelScope.launch {
+            _effect.emit(ResourceCreateContract.Effect.OpenFiles)
+        }
+        showUploadAttachmentBottomSheet(false)
+    }
+
+    private fun addAttachmentToCurrentAttachments(file: File) {
         _state.update {
             val addedAttachments = it.attachments.toMutableList().apply {
                 add(file)
