6f92c79ea54616932489bb4b5700255c57e113d5
muammarahlnn <21muammarahlan@gmail.com>
2024-02-03 22:50:18 +0800

    fix(resourcedetails): fix AttachmentsCard layout

--- feature/resourcedetails/src/main/java/com/muammarahlnn/learnyscape/feature/resourcedetails/composable/AttachmentsCard.kt
+++ feature/resourcedetails/src/main/java/com/muammarahlnn/learnyscape/feature/resourcedetails/composable/AttachmentsCard.kt
@@ -1,10 +1,12 @@
 package com.muammarahlnn.learnyscape.feature.resourcedetails.composable
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -43,14 +45,37 @@ internal fun AttachmentsCard(
 
             Spacer(modifier = Modifier.height(8.dp))
 
-            attachments.forEach { attachment ->
-                AttachmentItem(
-                    attachment = attachment,
-                    onAttachmentClick = onAttachmentClick,
-                    modifier = Modifier.fillMaxWidth()
-                )
+            for (i in attachments.indices step 2) {
+                Row(modifier = Modifier.fillMaxWidth()) {
+                    AttachmentItem(
+                        attachment = attachments[i],
+                        onAttachmentClick = onAttachmentClick,
+                        modifier = Modifier.weight(0.5f)
+                    )
+                    
+                    Spacer(modifier = Modifier.width(8.dp))
+                    
+                    if (i + 1 < attachments.size) {
+                        AttachmentItem(
+                            attachment = attachments[i + 1],
+                            onAttachmentClick = onAttachmentClick,
+                            modifier = Modifier.weight(0.5f)
+                        )
+                    } else {
+                        Spacer(modifier = Modifier.weight(0.5f))
+                    }
+                }
+
                 Spacer(modifier = Modifier.height(8.dp))
             }
+//            attachments.forEach { attachment ->
+//                AttachmentItem(
+//                    attachment = attachment,
+//                    onAttachmentClick = onAttachmentClick,
+//                    modifier = Modifier.fillMaxWidth()
+//                )
+//                Spacer(modifier = Modifier.height(8.dp))
+//            }
         }
     }
 }
