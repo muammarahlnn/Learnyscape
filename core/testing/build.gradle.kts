fb14bde3dbb1ebadcf880063efa7e36ee7e3b374
muammarahlnn <21muammarahlan@gmail.com>
2024-05-27 00:04:21 +0800

    feat(testing): add testing module

---
+++ core/testing/build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    id("learnyscape.android.library")
+    id("learnyscape.android.library.compose")
+}
+
+android {
+    namespace = "com.muammarahlnn.learnyscape.core.testing"
+}
+
+dependencies {
+    api(kotlin("test"))
+    api(libs.androidx.compose.ui.test)
+    api(project(":core:data"))
+    api(project(":core:model"))
+
+    debugApi(libs.androidx.compose.ui.testManifest)
+
+    implementation(libs.androidx.test.rules)
+    implementation(libs.hilt.android.testing)
+    implementation(libs.kotlinx.coroutines.test)
+    implementation(libs.kotlinx.datetime)
+    implementation(project(":core:common"))
+    implementation(project(":core:designsystem"))
+}
